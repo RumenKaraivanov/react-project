2d36746c8ca1a7c7bb09b92f60702ebfff05f821
RumenKaraivanov <rumbata_89.1989@abv.bg>
2022-06-18 19:55:05 +0300

    New login form added

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,46 +2,22 @@ import React from "react";
 
 const Login = () => {
     return (
-        <section className="vh-100">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-sm-6 text-black">
-
-                        <div className="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-
-                            <form style={{ width: "23rem" }}>
-
-                                <h3 className="fw-normal mb-3 pb-3" style={{ letterSpacing: "1px" }}>Log in</h3>
-
-                                <div className="form-outline mb-4">
-                                    <input type="email" id="form2Example18" className="form-control form-control-lg" />
-                                    <label className="form-label" htmlFor="form2Example18">Email address</label>
-                                </div>
-
-                                <div className="form-outline mb-4">
-                                    <input type="password" id="form2Example28" className="form-control form-control-lg" />
-                                    <label className="form-label" htmlFor="form2Example28">Password</label>
-                                </div>
-
-                                <div className="pt-1 mb-4">
-                                    <button className="btn btn-info btn-lg btn-block" type="button">Login</button>
-                                </div>
-
-                                <p className="small mb-5 pb-lg-2"><a className="text-muted" href="#!">Forgot password?</a></p>
-                                <p>Don't have an account? <a href="#!" className="link-info">Register here</a></p>
-
-                            </form>
-
-                        </div>
-
+        <div className="full-screen-container" style={{ backgroundImage: 'url(background_image.jpg)' }}>
+            <div className="login-container">
+                <h3 className="login-title">Welcome</h3>
+                <form>
+                    <div className="input-group">
+                        <label>Email</label>
+                        <input type="email" />
                     </div>
-                    <div className="col-sm-6 px-0 d-none d-sm-block">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img3.webp"
-                            alt="Login image" className="w-100 vh-100" style={{ objectFit: "cover", objectPosition: "left" }} />
+                    <div className="input-group">
+                        <label>Password</label>
+                        <input type="password" />
                     </div>
-                </div>
+                    <button type="submit" className="login-button">Sign In</button>
+                </form>
             </div>
-        </section>
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './components/Login.css'
 import App from './App';
 import Navbar from './components/NavBar';
 import reportWebVitals from './reportWebVitals';
