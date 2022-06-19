19f98c64dab32db4c9e66c4f51b2bca049514388
RumenKaraivanov <rumbata_89.1989@abv.bg>
2022-06-19 19:31:49 +0300

    Added create form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
+import Create from './components/Create';
 
 function App() {
   return (
-    <Register />
+    <Create />
   );
 }
 
