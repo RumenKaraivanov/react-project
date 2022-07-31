19354f138609ba0ce216667f5d644aebc9d6b2dd
RumenKaraivanov <rumbata_89.1989@abv.bg>
2022-07-31 17:34:57 +0300

    Added protected routes

---
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../../contexts/AuthContext";
+
+const PrivateRoute = () => {
+    const { auth } = useContext(AuthContext);
+
+    if (!auth.accessToke) {
+        return <Navigate to='/login' replace />;
+    };
+
+    return <Outlet />;
+
+};
+
+export default PrivateRoute;
